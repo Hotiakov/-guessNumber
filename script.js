60e58acdbe5738d3e153bfe736e696614104dbfb
Hotiakov <54938333+Hotiakov@users.noreply.github.com>
2021-08-05 13:47:33 +0300

    lesson05: done

---
+++ script.js
@@ -0,0 +1,36 @@
+const isNumber = function (n) {
+    return !isNaN(+n) && isFinite(n);
+};
+
+function startGame(secret) {
+    function guessNumber() {
+        let number = prompt("Угадай число от 1 до 100");
+        if (number == null) {
+            alert("Игра окончена");
+        }
+        else if (!isNumber(number)) {
+            alert("Введи число!")
+            guessNumber();
+        }
+        else {
+            number = +number;
+            if (number === secret) {
+                alert("Поздравляю, Вы угадали!!!");
+                return;
+            }
+            else if (number < secret) {
+                alert("Загаданное число больше");
+                guessNumber();
+            }
+            else {
+                alert("Загаданное число меньше");
+                guessNumber();
+            }
+        }
+    }
+    return guessNumber;
+}
+
+let guessNumber = startGame(71);
+
+guessNumber();
